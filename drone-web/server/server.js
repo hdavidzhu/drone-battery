e43e9e32fb068072df5dafb84296e0a6ba318352
Ashwin Dewan <ashwin.k.dewan@gmail.com>
2015-05-09 21:22:24 -0400

    added MQTT broker

--- drone-web/server/server.js
+++ drone-web/server/server.js
@@ -1,3 +1,43 @@
+var mosca = Meteor.npmRequire('mosca')
+
+var ascoltatore = {
+  //using ascoltatore
+  type: 'mongo',        
+  url: 'mongodb://localhost:3001/mqtt',
+  pubsubCollection: 'ascoltatori',
+  mongo: {}
+};
+
+var moscaSettings = {
+  port: 1883,
+  backend: ascoltatore,
+  persistence: {
+    factory: mosca.persistence.Mongo,
+    url: 'mongodb://localhost:3001/mqtt'
+  }
+};
+
+var server = new mosca.Server(moscaSettings);
+
+server.on('ready', setup);
+
+server.on('clientConnected', function(client) {
+    console.log('client connected', client.id);     
+});
+
+// fired when a message is received
+server.on('published', function(packet, client) {
+  console.log('Published', packet.payload.toString);
+});
+
+// fired when the mqtt server is ready
+function setup() {
+  console.log('Mosca server is up and running')
+}
+
+
+
+
 // On server startup, create some players if the database is empty.
 if (Meteor.isServer) {
   console.log("Yo");
